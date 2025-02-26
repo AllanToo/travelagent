00e127a5c8584673b375a3e26cb0512b0f3a0a1d
Allan To <le.to@edu.sait.ca>
2025-02-25 20:34:26 -0700

    25/02

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,34 +1,36 @@
-import React from 'react'
-import '../css/Header.css'
-function Header() {
+import React from "react";
+import { FaFacebookF, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+const Header = () => {
   return (
-    <div className="nav-bar">
-    <div className="logo">
-      <img src="./logo.png" alt="logo" />
+    <div className="flex items-center justify-between w-full px-10 py-6 border-b md:px-14">
+      {/* Left Side - Book Online */}
+      <a href="#" className="text-blue-600 font-bold text-3xl">
+        <span className="text-5xl">B</span>ook<span className="font-normal">online</span>
+      </a>
+
+      {/* Middle - Social Media Icons */}
+      <div className="flex items-center gap-8 text-blue-600 text-3xl md:gap-10">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition duration-300">
+          <FaFacebookF className="text-4xl" />
+        </a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition duration-300">
+          <FaTwitter className="text-4xl" />
+        </a>
+        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition duration-300">
+          <FaLinkedin className="text-4xl" />
+        </a>
+      </div>
+
+      {/* Divider */}
+      <div className="h-8 w-[1px] bg-gray-300 hidden md:block"></div>
+
+      {/* Right Side - Phone Number */}
+      <a href="tel:8552295934" className="text-blue-800 font-bold text-3xl">
+        855-229-5934
+      </a>
     </div>
+  );
+};
 
-    <ul className="menu">
-      <li>
-        <a href="/">HOME</a>
-      </li>
-      
-      
-      <li>
-        <a href="#service">REVIEWS</a>
-      </li>
-      <li>
-        <a href="#hour">ABOUT US</a>
-      </li>
-      <li>
-        <a href="#hour">SERVICES</a>
-      </li>
-     
-      <li>
-        <a href="#contact">FREE CONSULTATION </a>
-      </li>
-    </ul>
-  </div>
-  )
-}
-
-export default Header
+export default Header;
